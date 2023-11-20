fcb04a0db407458d74cea517b859ebd5030dd913
Galang <galangs123@gmail.com>
2023-11-20 19:54:16 +0700

    bump

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -10,12 +10,43 @@ import { ArticleService } from "@services/articles.service";
 import { Comment, CommentParsed } from "@interfaces/comment.interface";
 import { CommentService } from "@services/comments.service";
 
-
-import { CreateArticleDto, UpdateArticleDto } from "@dtos/articles.dto";
 import { apiResponse } from "@utils/apiResponse";
 
 export class CommentController {
-  private article = Container.get(ArticleService);
+  private comment = Container.get(CommentService);
+  public getCommentsByArticle = asyncHandler(async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const { article_id } = req.params;
+    const response = await this.comment.getCommentsByArticleId(article_id);
+    res.status(200).json(apiResponse(200, "OK", "Get Comment Success", response.comments));
+  });
 
+  public createComment = asyncHandler(async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const { article_id } = req.params;
+    const author_id = req.user.pk;
+    const data = req.body;
+    const response = await this.comment.createComment(article_id, author_id, data);
+    res.status(200).json(apiResponse(200, "OK", "Create Comment Success", response));
+  });
+
+  public updateComment = asyncHandler(async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const { comment_id } = req.params;
+
+    const data = req.body;
+    const response = await this.comment.updateComment(comment_id, data);
+    res.status(200).json(apiResponse(200, "OK", "Update Comment Success", response));
+  });
+
+  public deleteComment = asyncHandler(async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const { comment_id } = req.params;
+    const response: boolean = await this.comment.deleteComment(comment_id);
+    res.status(200).json(apiResponse(200, "OK", "Delete Comment Success", response));
+  });
+
+  public likeComment = asyncHandler(async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const { comment_id } = req.params;
+    const user_id = req.user.pk;
+    const response = await this.comment.likeComment(comment_id, user_id);
+    res.status(200).json(apiResponse(200, "OK", "Like Comment Success", response));
+  });
 
 }
--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -25,7 +25,17 @@ export class CommentService {
     };
   }
 
-  public async getCommentsByArticleId(articleId: number): Promise<{ comments: CommentParsed[] }> {
+  public async getCommentById(commentId: number): Promise<CommentParsed> {
+    const comment = await DB.ArticlesComments.findOne({ where: { pk: commentId } });
+
+    if (!comment) {
+      throw new HttpException(false, 404, "Comment not found");
+    }
+
+    return this.commentParsed(comment);
+  }
+
+  public async getCommentsByArticleId(articleId: string): Promise<{ comments: CommentParsed[] }> {
     // Get Article by ID
     const article = await DB.Articles.findOne({ where: { pk: articleId } });
 
@@ -38,13 +48,65 @@ export class CommentService {
   }
 
 
-  public async createComments(articleId: number, authorId: number, comment: string): Promise<CommentParsed> {
+  public async createComment(articleId: string, authorId: number, comment: string): Promise<CommentParsed> {
+    // Get Article by ID
+    const article = await DB.Articles.findOne({ where: { pk: articleId } });
+
+    // Create Comment
     const createdComment = await DB.ArticlesComments.create({
-      article_id: articleId,
+      article_id: article.pk,
       author_id: authorId,
       comment: comment,
     });
 
     return this.commentParsed(createdComment);
   }
+
+  public async updateComment(commentId: string, comment: string): Promise<CommentParsed> {
+    // Get Comment by ID
+    const commentToUpdate = await DB.ArticlesComments.findOne({ where: { pk: commentId } });
+
+    try {
+      // Update Comment
+      const updatedComment = await commentToUpdate.update({ comment: comment });
+
+      return this.commentParsed(updatedComment);
+    }
+    catch (error) {
+      throw new HttpException(false, 500, "Internal Server Error");
+    }
+
+
+  }
+
+  public async deleteComment(commentId: string): Promise<boolean> {
+    // Get Comment by ID
+    const commentToDelete = await DB.ArticlesComments.findOne({ where: { pk: commentId } });
+
+    try {
+      // Delete Comment
+      await commentToDelete.destroy();
+
+      return true;
+    }
+    catch (error) {
+      throw new HttpException(false, 500, "Internal Server Error");
+    }
+  }
+
+  public async likeComment(commentId: string, userId: number): Promise<boolean> {
+    // Get Comment by ID
+    const commentToLike = await DB.ArticlesComments.findOne({ where: { pk: commentId } });
+
+    try {
+      // Update Comment
+      await commentToLike.increment("likes");
+
+      return true;
+    }
+    catch (error) {
+      throw new HttpException(false, 500, "Internal Server Error");
+    }
+  }
+
 }
